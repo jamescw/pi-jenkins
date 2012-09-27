d6438a74238ec8885ab1bbbb7e6d8295f2150497
James Cranwell-Ward <jcranwellward@gmail.com>
2012-09-27 18:57:58 +0100

    fixing recursion error

--- leds.py
+++ leds.py
@@ -3,9 +3,9 @@ import RPi.GPIO as GPIO
 class BiColourLed(object):
     def __init__(self, red_pin, green_pin):
         self.colours = {
-            'off': None,
-            'red': None,
-            'green': None,
+            'off': False,
+            'red': False,
+            'green': False,
         }
         self.red_pin = red_pin
         self.green_pin = green_pin
@@ -13,27 +13,39 @@ class BiColourLed(object):
         GPIO.setwarnings(False)
         GPIO.setup(red_pin, GPIO.OUT)
         GPIO.setup(green_pin, GPIO.OUT)
-        self._set_colour('off', True)
 
     def _set_colour(self, colour, value=None):
         if colour in self.colours:
             if value:
-                for key,value in self.colours.items():
-                    if key is colour:
-                        self.colours[key] = True
+                for k,v in self.colours.items():
+                    if v is colour:
+                        self.colours[k] = True
                     else:
-                        self.colours[key] = False
+                        self.colours[k] = False
                 GPIO.output(self.red_pin, self.colours['red'])
                 GPIO.output(self.green_pin, self.colours['green'])
             return self.colours[colour]
         else:
-            raise KeyError("This LED does not support colour '{0}'".format(colour))
+            raise KeyError("This LED does not support colour {0}".format(colour))
 
-    def __getattr__(self, colour):
-        return self._set_colour(colour)
+#   Ideally what I want to do but causes recursion
+#    def __getattr__(self, colour):
+#        return self._set_colour(colour)
+#
+#    def __setattr__(self, colour, value):
+#        return self._set_colour(colour, value)
 
-    def __setattr__(self, colour, value):
-        return self._set_colour(colour, value)
+    @property
+    def off(self, value=None):
+        return self._set_colour('off', value)
+
+    @property
+    def red(self, value=None):
+        return self._set_colour('red', value)
+
+    @property
+    def green(self, value = None):
+        return self._set_colour('green', value)
 
     def __repr__(self):
         return "LED is state is: ".format(self.colours)
